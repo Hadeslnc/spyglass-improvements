124b206b9ffc20c5aed67013c6ecb9af5328b4de
Juan Carlos <juancarloscp52@gmail.com>
2024-06-01 12:31:22 +0200

    Add spyglass button to dynamic grid (fix #49

--- src/main/java/me/juancarloscp52/spyglass_improvements/mixin/OptionsScreenMixin.java
+++ src/main/java/me/juancarloscp52/spyglass_improvements/mixin/OptionsScreenMixin.java
@@ -20,6 +20,8 @@ package me.juancarloscp52.spyglass_improvements.mixin;
 import me.juancarloscp52.spyglass_improvements.client.SpyglassConfigurationScreen;
 import me.juancarloscp52.spyglass_improvements.client.SpyglassImprovementsClient;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.OptionsScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -27,6 +29,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(OptionsScreen.class)
 public class OptionsScreenMixin extends Screen {
@@ -35,10 +38,10 @@ public class OptionsScreenMixin extends Screen {
         super(title);
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void insertEntropySettingsButton(CallbackInfo ci) {
+    @Inject(method = "init", at = @At(value = "INVOKE",target = "Lnet/minecraft/client/gui/layouts/HeaderAndFooterLayout;addToContents(Lnet/minecraft/client/gui/layouts/LayoutElement;)Lnet/minecraft/client/gui/layouts/LayoutElement;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void addBedrockIfyButtonBelowSliders(CallbackInfo ci, LinearLayout linearLayout, LinearLayout linearLayout2, GridLayout gridLayout, GridLayout.RowHelper rowHelper) {
         if(!SpyglassImprovementsClient.getInstance().settings.hideSettingsButton)
-            this.addRenderableWidget(Button.builder(Component.translatable("options.spyglass-improvements.title"), button -> this.minecraft.setScreen(new SpyglassConfigurationScreen(this))).pos(this.width / 2 - 155, this.height / 6 + 21 - 6).width(310).build());
+            rowHelper.addChild(Button.builder(Component.translatable("options.spyglass-improvements.title"), button -> this.minecraft.setScreen(new SpyglassConfigurationScreen(this))).build());
     }
 
 }
